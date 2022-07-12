7c3cfa48a7f83ca254ebc8dfe8c80ef2cfe7fc3f
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-07-12 12:44:47 +0200

    Setup menu

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,48 @@
+const createMenu = () => {
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+
+  const check = document.createElement("p");
+  check.textContent = "Menu";
+
+  const createPizzaCard = (name, info) => {
+    const pizzaCard = document.createElement("div");
+    pizzaCard.classList.add("menu-card");
+
+    const pizzaName = document.createElement("p");
+    pizzaName.classList.add("card-title");
+    pizzaName.textContent = name;
+
+    const pizzaInfo = document.createElement("p");
+    pizzaInfo.classList.add("card-description");
+    pizzaInfo.textContent = info;
+
+    pizzaCard.appendChild(pizzaName);
+    pizzaCard.appendChild(pizzaInfo);
+
+    return pizzaCard;
+  };
+
+  menu.appendChild(
+    createPizzaCard("Margharita", "Just a dough with pomodoro sauce")
+  );
+  menu.appendChild(
+    createPizzaCard("Margharita", "Just a dough with pomodoro sauce")
+  );
+  menu.appendChild(
+    createPizzaCard("Margharita", "Just a dough with pomodoro sauce")
+  );
+  menu.appendChild(
+    createPizzaCard("Margharita", "Just a dough with pomodoro sauce")
+  );
+
+  return menu;
+};
+
 const loadMenu = () => {
   const main = document.querySelector("#main");
-  const menu = document.createElement("p");
-  menu.textContent = "Menu";
 
-  main.appendChild(menu);
+  main.appendChild(createMenu());
 };
 
 export default loadMenu;
