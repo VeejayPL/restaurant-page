0f95257c8a372c8008b66f286a16425ad43c269f
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-07-16 13:13:33 +0200

    Add fadeInText function

--- src/home.js
+++ src/home.js
@@ -1,13 +1,31 @@
+"use strict";
 const createHome = () => {
   const home = document.createElement("div");
   home.classList.add("home");
 
-  const check = document.createElement("p");
-  check.textContent = "Home";
-
-  home.appendChild(check);
+  home.appendChild(fadeInText("Kocham Asie jak pizze z frontiery", "p", 50));
 
   return home;
+};
+
+const fadeInText = (text, element, speed) => {
+  const paragraph = document.createElement(element);
+
+  const characters = Array.from(text);
+  console.log(characters);
+
+  characters.forEach((character, index) => {
+    const span = document.createElement("span");
+    span.textContent = characters[index];
+    span.style.animationDelay = `${index * speed}ms`;
+    paragraph.appendChild(span);
+  });
+
+  const removeSpanTag = () => {
+    paragraph.textContent = text;
+  };
+  setTimeout(removeSpanTag, 3000);
+  return paragraph;
 };
 
 const loadHome = () => {
