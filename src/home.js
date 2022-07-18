72e00d2fe79580d775dd1346e6764f575d983733
VeejeyPL <wojciech.jurkowski4@gmail.com>
2022-07-18 14:27:05 +0200

    Add home section

--- src/home.js
+++ src/home.js
@@ -3,13 +3,33 @@ const createHome = () => {
   const home = document.createElement("div");
   home.classList.add("home");
 
-  home.appendChild(fadeInText("Kocham Asie jak pizze z frontiery", "p", 50));
+  const info = document.createElement("div");
+  info.classList.add("home-info");
+
+  const hours = document.createElement("div");
+  hours.classList.add("home-hours");
+
+  hours.appendChild(addText("7 DAYS A WEEK", "p"));
+  hours.appendChild(addText("4PM - 9PM", "p"));
+  hours.appendChild(addText("DINE-IN", "p"));
+
+  info.appendChild(
+    fadeInText(
+      "RISTORANTE PIZZA & WINE. Sandwiches, provisions, wine shop, and more.",
+      "p",
+      25
+    )
+  );
+
+  home.appendChild(info);
+  home.appendChild(hours);
 
   return home;
 };
 
 const fadeInText = (text, element, speed) => {
   const paragraph = document.createElement(element);
+  paragraph.classList.add(`${element}-animated`);
 
   const characters = Array.from(text);
   console.log(characters);
@@ -24,7 +44,14 @@ const fadeInText = (text, element, speed) => {
   const removeSpanTag = () => {
     paragraph.textContent = text;
   };
-  setTimeout(removeSpanTag, 3000);
+  setTimeout(removeSpanTag, 4000);
+  return paragraph;
+};
+
+const addText = (text, element) => {
+  const paragraph = document.createElement(element);
+  paragraph.textContent = text;
+
   return paragraph;
 };
 
