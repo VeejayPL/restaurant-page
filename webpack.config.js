5dc9227d57e2add8a61ec030f4785ddcd5d4fd27
VeejeyPL <wojciech.jurkowski4@gmail.com>
2023-02-22 11:05:10 +0100

    Add web dev server

--- webpack.config.js
+++ webpack.config.js
@@ -8,4 +8,9 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+  },
 };
